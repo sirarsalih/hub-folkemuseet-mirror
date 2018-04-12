eb37a30b1596e378e5a886280736ce1231adc401
Sirar Salih <sirar.salih@makingwaves.com>
2018-04-12 12:39:07 +0200

    Remove dialog with add and cancel button functionalities.

--- wwwroot/js/app.js
+++ wwwroot/js/app.js
@@ -34,26 +34,11 @@
    *
    ****************************************************************************/
 
-  document.getElementById('butAddCity').addEventListener('click', function() {
-    // Add the newly selected city
-    var select = document.getElementById('selectCityToAdd');
-    var selected = select.options[select.selectedIndex];
-    var key = selected.value;
-    var label = selected.textContent;
-    if (!app.selectedCities) {
-      app.selectedCities = [];
-    }
-    app.getForecast(key, label);
-    app.selectedCities.push({key: key, label: label});
-    app.saveSelectedCities();
-    app.toggleAddDialog(false);
-  });
-
-  document.getElementById('butAddCancel').addEventListener('click', function() {
-    // Close the add new city dialog
-    app.toggleAddDialog(false);
-  });
-
+  // Example of an event listener for a button
+  //document.getElementById('butAddCancel').addEventListener('click', function() {
+  //  // Close the add new city dialog
+  //  app.toggleAddDialog(false);
+  //});
 
   /*****************************************************************************
    *
--- wwwroot/service-worker.js
+++ wwwroot/service-worker.js
@@ -34,16 +34,6 @@ var filesToCache = [
   '/images/wind.png'
 ];
 
-self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(filesToCache);
-    })
-  );
-});
-
 self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
   e.waitUntil(
