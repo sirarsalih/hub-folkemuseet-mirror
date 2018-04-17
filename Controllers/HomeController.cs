9295a762f01e759806a5f05e066b121065601a8c
Sirar Salih <sirar.salih@makingwaves.com>
2018-04-17 13:55:02 +0200

    Update locations JSON with current user position.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,5 +9,15 @@ namespace NorskFolkemuseum.Controllers
             return View();
         }
 
+        public void UpdateJSON(string lat, string lng)
+        {
+            const string pathToJSON = "wwwroot/data/locations.json";
+            var jsonStr = System.IO.File.ReadAllText(pathToJSON);
+            dynamic jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject(jsonStr);
+            jsonObj["lat"] = lat;
+            jsonObj["lng"] = lng;
+            var output = Newtonsoft.Json.JsonConvert.SerializeObject(jsonObj, Newtonsoft.Json.Formatting.Indented);
+            System.IO.File.WriteAllText(pathToJSON, output);
+        }
     }
 }
