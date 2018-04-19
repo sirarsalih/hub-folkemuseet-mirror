d11b818803920851b91080bd15b3e71fc3965b76
Sirar Salih <sirar.salih@makingwaves.com>
2018-04-19 15:02:01 +0200

    Check if sound exists in location, if not, then display a message saying so.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 
 namespace NorskFolkemuseum.Controllers
 {
@@ -9,15 +11,23 @@ namespace NorskFolkemuseum.Controllers
             return View();
         }
 
-        public void UpdateJSON(string lat, string lng)
+        public bool UpdateJSON(string lat, string lng)
         {
+            var locationHasSound = false;
             const string pathToJSON = "wwwroot/data/locations.json";
             var jsonStr = System.IO.File.ReadAllText(pathToJSON);
             dynamic jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject(jsonStr);
             jsonObj["lat"] = lat;
             jsonObj["lng"] = lng;
+            foreach (var sound in jsonObj.sounds) {
+                if(sound.lat.Value.Equals(lat) && sound.lng.Value.Equals(lng)) {
+                    locationHasSound = true;
+                    break;
+                }
+            }
             var output = Newtonsoft.Json.JsonConvert.SerializeObject(jsonObj, Newtonsoft.Json.Formatting.Indented);
             System.IO.File.WriteAllText(pathToJSON, output);
+            return locationHasSound;
         }
     }
 }
